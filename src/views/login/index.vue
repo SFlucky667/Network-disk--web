cab162e84987e7bd8b2af657ee23125101fe32e7
lenovo <sf.lucky667@gmail.com>
2025-01-21 12:51:14 +0800

    登录页面（2）

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,9 @@
 <script setup lang="ts">
 import {reactive} from "vue";
-import { ElMessage, type FormRules } from 'element-plus';
-
-type RuleForm=IAuthLoginRequest;
-const form=reactive<RuleForm>({
-  username: '',
-  password: '',
-}) ;
-const rules=reactive<RuleForm>({
-  username: [{required:true,message:"请输入用户名",trigger:'blur'}],
-  password: [{requied:true,message:"请输入密码",trigger:'blur'}],
-});
-const formRules=ref();
+const form = reactive({
+  userName: "",
+  password: "",
+})
 </script>
 
 <template>
@@ -19,7 +11,7 @@ const formRules=ref();
     <div class="banner">
     </div>
     <div class="login_from">
-      <el-form ref="formRef" :model="form" :rules="rules">
+      <el-form  :model="form" >
         <el-form-item prop="username">
           <el-input v-model="form.userName" placeholder="用户名"/>
         </el-form-item>
@@ -32,9 +24,7 @@ const formRules=ref();
         <el-form-item class="item_btn">
         <el-button style="width: 100%" type="primary">登录</el-button>
         </el-form-item>
-      <div class="register_link">
-        <router-link to="/register">没账号？立即注册</router-link>
-      </div>
+
       </el-form>
     </div>
   </div>
@@ -44,7 +34,7 @@ const formRules=ref();
  .disk_login {
    width: 500px;
    height: 406px;
-   background-color: white;
+   background-color: rgba(0, 253, 232, 0.16);
    border-radius: 5px;
    overflow: hidden;
    box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.05);
@@ -52,16 +42,16 @@ const formRules=ref();
    .banner {
      height: 140px;
      width: 100%;
-     background-color: #0084fd;
+     background-color: #0094fd;
 
      &::after {
          content: "Network disk";
        position: absolute;
-       top:50%;
+       top:30%;
        left:50%;
        transform: translate(-50%,-50%);
-       color: darkblue;
-       font-size: 2rem;
+       color: #fd0022;
+       font-size: 3rem;
      }
    }
    .login_from {
